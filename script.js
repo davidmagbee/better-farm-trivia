cc356dde6fab7254ffd776d53f12fddf0ada2892
magbee <david.magbee@me.com>
2019-12-10 14:01:57 -0500

    pseudocode for JS started...

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-// Questions
+// Develop Questions
+// Create question array const
+// Use OOP to create an Object with key:value pairs for Qs/As
+// Create class for Question (question, choices, answer)
+// Create class for actual trivia question quiz
+// 
+
