04f6d0e7ad8a88ef6fd245f62a270fe4e9e8da8d
magbee <david.magbee@me.com>
2019-12-10 15:53:23 -0500

    back step to simplify further for MVP. New pseudocode for MVP, Q&A consts created.

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
+//// Initial conceptual process...
 // Develop Questions
 // Create question array const
 // Use OOP to create an Object with key:value pairs for Qs/As
 // Create class for Question (question, choices, answer)
 // Create class for actual trivia question quiz
-// 
+
+// MVP process
+// Create questions (3-5) - use array - DONE
+// Create answers (=) - use array - DONE
+// Create Next question button - DONE
+// Populate Qs/As using DOM manipulation
+// INTRO MODAL WINDOW???
+// POPULATE trivia game
+//// Questions and Answers divs created into game box (DOM) 
+// Create event listeners for answer check and response
+// Create answer check - use boolean logic to check answer
+// Next question functionality
+
+const questions = [
+    "How many horses live at Better Farm?",
+    "How many pigs live at Better Farm?",
+    "How many chickens live at Better Farm?",
+    "How many alpacas live at Better Farm?",
+    "How many dogs live at Better Farm?",
+    "How many bee hives host bees at Better Farm?",
+    "How many humans regularly live at Better Farm?"
+]
+
+const answers = [
+    2, 
+    3,
+    25,
+    8,
+    3,
+    1,
+    2
+]
 
